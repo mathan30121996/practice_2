a677e131f63717d01aaf3e5803ae362d0b7d5196
mathan30121996 <mathancietcbe@gmail.com>
2022-04-07 06:44:40 +0530

    yes we are done

---
+++ src/com/practice/String_Methods.java
@@ -0,0 +1,11 @@
+package com.practice;
+
+public class String_Methods {
+	public static void main(String[] args) {
+		String s= "Mathan Panneerselvam 30";
+		int length = s.length();
+		System.out.println(length);
+		System.out.println(s.toUpperCase());
+		
+	}
+}
